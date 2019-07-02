7f5a3009430c42b8b5a9bf8489d907c2035e0cfa
Ning Wei <ning.wei@int-trade.de>
2019-07-02 18:02:22 +0200

    Anpassungen inkl. ORM annotations

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,34 +8,51 @@
 
 namespace AppBundle\Entity;
 
-
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
 class User implements UserInterface
 {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
     public function getUsername()
     {
-        // TODO: Implement getUsername() method.
+        return $this->email;
     }
 
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
     {
-        // TODO: Implement getPassword() method.
     }
 
     public function getSalt()
     {
-        // TODO: Implement getSalt() method.
     }
 
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
     }
 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
 }
