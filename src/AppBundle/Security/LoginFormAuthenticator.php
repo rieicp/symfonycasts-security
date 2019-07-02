4d2732cb089a031d65ec9dc95cbf3547cf9176d8
Ning Wei <ning.wei@int-trade.de>
2019-07-02 19:50:58 +0200

    使用Form对象处理request，完善getCredentials()方法

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Security;
 
+use AppBundle\Form\LoginForm;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -23,7 +24,18 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function getCredentials(Request $request)
     {
-        // TODO: Implement getCredentials() method.
+        $isFormSubmit = $request->attributes->get('_route') === 'security_login' && $request->isMethod('POST');
+        //或者
+        //$isFormSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
+        if (!$isFormSubmit) {
+            return; //若非login表单提交，则直接返回，Authenticator不再执行后续方法
+        }
+
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+
+        return $data;//若返回任何非空数据，Authenticator会继续执行后续方法
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
