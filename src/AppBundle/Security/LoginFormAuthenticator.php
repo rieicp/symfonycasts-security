3331bc76ac7ffebd4d44a62372f9b6843e5d2cde
Ning Wei <ning.wei@int-trade.de>
2019-07-06 08:26:40 +0200

    查看SecurityController中的$authenticationUtils->getLastUsername()， 上次使用的用户名会从session中读取，键名为 'Security::LAST_USERNAME'， 因此在LoginFormAuthenticator::getCredentials()中添加代码， 在session中设置上次使用的用户名。

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -46,6 +47,11 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $form = $this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
         $data = $form->getData();
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $data['_username']
+        );
 
         return $data;//若返回任何非空数据，Authenticator会继续执行后续getUser()方法
     }
