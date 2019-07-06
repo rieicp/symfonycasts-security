1690e99e96ebb2605e04b34e43060880fc193860
Ning Wei <ning.wei@int-trade.de>
2019-07-06 18:42:09 +0200

    创建register的twig模板，修改Controller，创建form，并赋值给twig模板， 然后在login模板中添加register的链接，访问register链接， 出现register表单，但密码及重复的标题为'first'和'second'，需要修改！

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,15 +8,21 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserRegistrationForm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class UserController
+class UserController extends Controller
 {
     /**
      * @Route("/register", name="user_register")
      */
     public function registerAction()
     {
-        
+        $form = $this->createForm(UserRegistrationForm::class);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
