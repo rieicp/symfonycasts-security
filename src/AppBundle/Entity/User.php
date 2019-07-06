1568c50c1702b0e2ce0fb0ac83f32f2bbf0fa182
Ning Wei <ning.wei@int-trade.de>
2019-07-06 13:24:10 +0200

    增加 User::roles，修改 fixtures.yml， 然后运行 migrations:diff, migrations:migrate，fixtures:load

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -39,6 +39,11 @@ class User implements UserInterface
      */
     private $plainPassword;
 
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $roles = [];
+
     public function getUsername()
     {
         return $this->email;
@@ -46,7 +51,13 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        $roles = $this->roles;
+
+        if (!in_array('ROLE_USER', $roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return $roles;
     }
 
     public function getPassword()
@@ -94,4 +105,13 @@ class User implements UserInterface
         // when changing the plainPassword
         $this->password = null;
     }
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
 }
