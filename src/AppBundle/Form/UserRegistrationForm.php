27f4e88c5f5dc2b1d92184f94665dafc474fa67e
Ning Wei <ning.wei@int-trade.de>
2019-07-06 18:29:15 +0200

    接着定义UserRegistrationForm，添加 email为EmailType类型，plainPassword 为RepeatedType类（因为需要repeat密码）， 注意在第三个参数中设置'type' => PasswordType::class，即， 密码及重复框都使用'密码'的类型（显示星号隐藏明文）

--- src/AppBundle/Form/UserRegistrationForm.php
+++ src/AppBundle/Form/UserRegistrationForm.php
@@ -4,6 +4,9 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +14,11 @@ class UserRegistrationForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        
+        $builder
+            ->add('email', EmailType::class)
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
