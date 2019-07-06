f60f996d0ee6f6c78b54ea17f8eab576136a9068
Ning Wei <ning.wei@int-trade.de>
2019-07-06 18:25:42 +0200

    创建路由 /register 所对应的 Controller，并且创建 UserRegistrationForm类，在configureOptions()中通过 $resolver->setDefaults()，将'data_class'绑定为User类

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nwe
+ * Date: 06.07.19
+ * Time: 18:21
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class UserController
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction()
+    {
+        
+    }
+}
---
+++ src/AppBundle/Form/UserRegistrationForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserRegistrationForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class
+        ]);
+    }
+
+}
